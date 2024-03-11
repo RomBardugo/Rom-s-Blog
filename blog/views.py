8bc48462177f486c49214e6b39989146bd1d347a
RomBardugo <Rombardugo1@gmail.com>
2024-03-11 11:10:22 +0200

    add all files

---
+++ blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Post
+
+
+def main_page(request):
+    latest_posts = Post.objects.all().order_by("-date")[:3]
+    return render(request, "blog/index.html", {
+        'posts': latest_posts
+    })
+
+def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
+    return render(request, "blog/all-posts.html", {
+        "all_posts" : all_posts
+    })
+
+def show_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {
+        "post" : post,
+        "post_tags": post.tags.all()
+    })
+ 
